103bd3a286fe5cca321f39a83f326a91684ca96c
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-26 17:24:51 +0200

    aggiunto middleware auth

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('admin.home');
+Route::get('/home', 'HomeController@index')->middleware('auth')->name('admin.home');
