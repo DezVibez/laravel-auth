fb4500699a0549292f0ab8df1026c0c1f02efe38
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-27 18:49:08 +0200

    render di posts a schermo ok

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Model\Post;
 
 class PostController extends Controller
 {
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,49 @@
-extends('layouts.app')
+@extends('layouts.app')
+
+@section('content')
+
+<header>
+    <h1>
+        Post List
+    </h1>
+
+</header>
+
+<main>
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titolo </th>
+      <th scope="col">Slug</th>
+      <th scope="col">Creato il</th>
+      <th scope="col">Modificato il</th>
+      <th scope="col">Azioni</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse($posts as $post)
+    <tr>
+      <th scope="row">{{ $post->id }}</th>
+      <td>{{ $post->title }}</td>
+      <td>{{ $post->slug }}</td>
+      <td>{{ $post->created_at }}</td>
+      <td>{{ $post->updated_at }}</td>
+      <td></td>
+    </tr>
+    @empty
+    <tr>
+      <td scope="row"> <h3 class="text-center"> Nessun post </h3> </td>
+    </tr>
+    @endforelse
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</main>
+
+@endsection
