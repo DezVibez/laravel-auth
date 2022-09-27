3bc46fecbc8566031d6ae0945ae26ea5618ce902
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-27 14:49:17 +0200

    forse ci siamo

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ route('admin.home') }}">Home</a>
+                        <a href="{{ url('/') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
