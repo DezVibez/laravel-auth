c0e19eee6f44bf3e3d5c88c3a423124fd8803105
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-28 16:32:42 +0200

    finito edit form unificato

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,51 +2,12 @@
 
 @section('content')
 
-<header class="container">
-    <h1>
-        Crea un nuovo Post
-    </h1>
-</header>
-<div class="container">
-    
-    <form action="{{ route('admin.posts.store') }}" method="POST">
-        @csrf
-        
-        <div class="form-group">
-            <label for="title">Titolo</label>
-            <input type="text" class="form-control"
-             id="title" required minlenght="5" maxlenght="50" value="{{ old('title') }}" name="title">
-        </div>
+    <header class="container">
+        <h1>
+            Crea un nuovo Post
+        </h1>
+    </header>
 
-        <div class="form-group">
-            <label for="content">Contenuto</label>
-            <textarea class="form-control" id="content" name="content">
-                {{ old('content') }}
-            </textarea>
-        </div>
+    @include('includes.admin.posts.form')
 
-        <div class="form-group">
-            <label for="image">Immagine</label>
-            <input type="url" class="form-control"
-             id="image" value="{{ old('image') }}" name="image">
-        </div>
-
-        <hr>
-    
-        
-        
-        <hr>
-        
-        <footer class="d-flex justify-content-between">
-            <a href="{{ route('admin.posts.index') }}" class="btn btn-secondary">
-                Torna indietro <i class="fa-solid fa-rotate-left"></i>
-            </a>
-            
-            <button type="submit" class="btn btn-success">
-                Crea <i class="fa-solid fa-plus"></i>
-            </button>
-        </footer>
-    </form>
-    
-</div>
-    @endsection
+@endsection
---
+++ resources/views/includes/admin/posts/form.blade.php
@@ -0,0 +1,47 @@
+<div class="container">
+
+    @if($post->exists)
+    <form action="{{ route('admin.posts.update', $post) }}" method="POST">
+        @method('PUT')
+    @else
+    <form action="{{ route('admin.posts.store') }}" method="POST">
+    @endif
+        @csrf
+        
+        <div class="form-group">
+            <label for="title">Titolo</label>
+            <input type="text" class="form-control"
+             id="title" required minlenght="5" maxlenght="50" value="{{ old('title', $post->title)  }}" name="title">
+        </div>
+
+        <div class="form-group">
+            <label for="content">Contenuto</label>
+            <textarea class="form-control" id="content" name="content">
+                {{ old('content', $post->content) }}
+            </textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="image">Immagine</label>
+            <input type="url" class="form-control"
+             id="image" value="{{ old('image', $post->image) }}" name="image">
+        </div>
+
+        <hr>
+    
+        
+        
+        <hr>
+        
+        <footer class="d-flex justify-content-between">
+            <a href="{{ route('admin.posts.index') }}" class="btn btn-secondary">
+                Torna indietro <i class="fa-solid fa-rotate-left"></i>
+            </a>
+            
+            <button type="submit" class="btn btn-success">
+                Crea <i class="fa-solid fa-plus"></i>
+            </button>
+        </footer>
+    </form>
+    
+</div>
