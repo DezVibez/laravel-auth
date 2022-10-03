b2214ac706b08fea1f4329fb944793e61d178a36
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-10-03 17:18:30 +0200

    gestito old in input

--- resources/views/includes/admin/posts/form.blade.php
+++ resources/views/includes/admin/posts/form.blade.php
@@ -53,8 +53,14 @@
             <h4>Tags</h4>
             @foreach($tags as $tag)
             <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" >
-                <label class="form-check-label">{{ $tag->label }}</label>
+                <input type="checkbox" 
+                    class="form-check-input" 
+                    id=" tag-{{ $tag->label }}" 
+                    name="tags[]" 
+                    value="{{ $tag->id }}"
+                    @if( in_array( $tag->id, old('tags', []) )) checked @endif
+                    > 
+                <label class="form-check-label" for=" tag-{{ $tag->label }}">{{ $tag->label }}</label>
             </div>
             @endforeach
         @endif
